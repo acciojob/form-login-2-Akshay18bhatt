5d0008a4f9d630ba33f892f46d7bbb2761ba1ca7
Akshay Bhatt <120771512+Akshay18bhatt@users.noreply.github.com>
2023-11-13 19:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button=document.querySelector("button");
+let form= document.querySelector("form");
+button.addEventListener("click" ,onclick);
+function onclick() {
+	alert(console.log(form))
+}
